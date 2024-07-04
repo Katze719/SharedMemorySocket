83facd611f2b306675c32396cc0ab33ce6a7c57c
Katze719 <katze719@users.noreply.github.com>
2024-07-04 21:55:23 +0200

    feat: :sparkles: add flags

--- SharedMemorySocket/protocol.py
+++ SharedMemorySocket/protocol.py
@@ -1,5 +1,16 @@
 import ctypes
+import enum
 
+class SHMFlags(enum.Flag):
+    FIN = 0x01 # 0b00000001
+    SYN = 0x02 # 0b00000010
+    RST = 0x04 # 0b00000100
+    PSH = 0x08 # 0b00001000
+    ACK = 0x10 # 0b00010000
+    URG = 0x20 # 0b00100000
+    ECE = 0x40 # 0b01000000
+    CWR = 0x80 # 0b10000000
+    
 # header = 14 bytes, gives us 1010 bytes for the payload
 class SHMHeader(ctypes.Structure):
     _fields_ = [
